5e8cc647439da95c079aa27d1f342f36495352c9
aristo-13 <oseirichmond310@gmail.com>
2023-11-13 10:11:25 +0000

    new changes

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react'
 import { FaArrowRight } from 'react-icons/fa6'
-import Fashionsta from '../../public/fashionsta.jpg'
 
-function CategoryCard( {category} ) {
+
+function CategoryCard( {category,img} ) {
+ const [image,setImg] = useState('')
+
+
+ useEffect(() => {
+    setTimeout(() => {
+        setImg(img)
+    },5000)
+    
+ },[])
+
+
   return (
-    <div className="p-4 min-h-[300px] w-[min(100%,250px)] rounded-2xl bg-cover bg-center relative flex flex-col justify-end items-center shadow-sm border-4 overflow-hidden border-white cursor-pointer hover:scale-[1.05] hover:z-[1] duration-500"  style={{backgroundImage: `url()`}}>
-         <span className='overlay absolute w-full h-full left-0 top-0 z-10 bg-black/60'></span>
-         <button className='w-[90%] bg-white rounded-xl flex justify-center items-center p-2 gap-2 z-10'>{category} <span><FaArrowRight /></span></button>
-    </div>
+    <>
+
+         {!image && <div className='p-4 min-h-[300px] w-[min(100%,250px)] rounded-2xl shadow-sm border-4 overflow-hidden border-white cursor-pointer hover:scale-[1.05] hover:z-[1] duration-500 bg-gray-500 animate-pulse'></div>}
+         {image && <div className="p-4 min-h-[300px] w-[min(100%,250px)] rounded-2xl bg-cover bg-center relative flex flex-col justify-end items-center shadow-sm border-4 overflow-hidden border-white cursor-pointer hover:scale-[1.05] hover:z-[1] duration-500"  style={{backgroundImage: `url(${image})`}}>
+            <span className='overlay absolute w-full h-full left-0 top-0 z-10 bg-black/60'></span>
+            <button className='w-[90%] bg-white rounded-xl flex justify-center items-center p-2 gap-2 z-10 hover:bg-DarkBlue hover:text-white duration-200'>{category} <span><FaArrowRight /></span></button>
+        </div>}
+    </>
+   
   )
 }
 
--- src/components/CuratedPicks.jsx
+++ src/components/CuratedPicks.jsx
@@ -5,15 +5,19 @@ function CuratedPicks() {
    const categories = [
     {
         category: "electronics",
+        img: "https://images.unsplash.com/photo-1585298723682-7115561c51b7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhlYWRwaG9uZXN8ZW58MHx8MHx8fDA%3D",
     },
     {
         category: "Men's Clothing",
+        img: "https://images.unsplash.com/photo-1594938298603-c8148c4dae35?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1lbidzJTIwY2xvdGhpbmd8ZW58MHx8MHx8fDA%3D",
     },
     {
         category: "Women's Clothing",
+        img: "https://images.unsplash.com/photo-1532453288672-3a27e9be9efd?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fFdvbWVuJ3MlMjBDbG90aGluZ3xlbnwwfHwwfHx8MA%3D%3D"
     },
     {
         category: "Jewelery",
+        img: "https://images.unsplash.com/photo-1524805444758-089113d48a6d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2h8ZW58MHx8MHx8fDA%3D"
     },
    ]
 
@@ -23,7 +27,7 @@ function CuratedPicks() {
          <div className='w-full md:w-[95%] flex justify-center items-center  flex-wrap gap-2'>
         {
             categories.map((category) => (
-                <CategoryCard category={category.category}/>
+                <CategoryCard category={category.category} img={category.img}/>
             ))
         }
         </div>
